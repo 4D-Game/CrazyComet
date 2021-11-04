90a199a5014ba1bf2fd0fa04ff578fedebf8ab05
Lukas Schüttler <lukas.schuettler@ise-technology.com>
2021-11-04 22:47:36 +0100

    ADD turretHAL implementation for servo

--- src/hardware/turrets.py
+++ src/hardware/turrets.py
@@ -1,4 +1,5 @@
 import logging
+import RPi.GPIO as GPIO
 
 
 class TurretHAL():
@@ -11,6 +12,11 @@ class TurretHAL():
 
     def __init__(self, pin: int = 17):
         self.pin = pin
+
+        GPIO.setup(pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(pin, 50)
+        self.pwm.start(self.__mapPosToPWM__(0))
+
         logging.info(f"Init turret at Pin {pin}")
 
     def setPosition(self, pos: int = 50):
@@ -21,11 +27,31 @@ class TurretHAL():
                 force: Turretposition between 0 and 100
         """
 
-        logging.info(f"Turret triggered with force {pos}")
+        self.pwm.ChangeDutyCycle(self.__mapPosToPWM__(pos))
+        logging.info(f"Turret moved to pos {pos}")
 
     def close():
         """
             Called when the Interface is not needed anymore
         """
 
+        GPIO.cleanup()
         logging.info(f"Turret interface closed")
+
+    def __mapPosToPWM__(self, pos: int) -> int:
+        """
+            Map a given position to PWM duty cycle
+
+            Arguments:
+                pos: Position between 0 and 100
+        """
+
+        min_deg = 0
+        max_deg = 180
+
+        pwm_range = (5, 10)
+
+        min_pwm = pwm_range[0] + (180 / (pwm_range[1] - pwm_range[0]) * min_deg)
+        max_pwm = pwm_range[0] + (180 / (pwm_range[1] - pwm_range[0]) * max_deg)
+
+        return min_pwm + (100 / (max_pwm - min_pwm) * pos)
