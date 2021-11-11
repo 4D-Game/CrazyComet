b5cb86dcf892b500bedbd5c54bb6298245078b34
Lukas Schüttler <lukas.schuettler@ise-technology.com>
2021-11-11 12:37:27 +0100

    ADD shooting for turrets

--- src/controls/turrets.py
+++ src/controls/turrets.py
@@ -1,10 +1,13 @@
+from time import sleep
 from typing import List
-from ..hardware.turrets import TurretHAL
+
+from surrortg.inputs.switch import Switch
+from ..hardware.turrets import TurretPosHAL, TurretShootHAL
 from surrortg.inputs import MouseJoystick
 import logging
 
 
-class TurretControl(MouseJoystick):
+class TurretPositionControl(MouseJoystick):
     """
         Joystick wich controls the turrets
     """
@@ -13,9 +16,9 @@ class TurretControl(MouseJoystick):
         pin = 0  # Read config
 
         # init controller
-        self.controller = TurretHAL(pin)
+        self.controller = TurretPosHAL(pin)
 
-        logging.info("Turret initialized")
+        logging.info("Turret Positioncontrol initialized")
 
     async def handle_coordinates(self, x, y, seat: int = 0, dx=None, dy=None):
         """
@@ -30,6 +33,39 @@ class TurretControl(MouseJoystick):
         self.controller.setPosition(pos)
 
         logging.info(f"Position turret for seat {seat}")
+
+    def shutdown(self, seat: int = 0):
+        self.controller.close()
+
+
+class TurretShootControl(Switch):
+    """
+        Switch wich controls the shooting of the turrets
+    """
+
+    shooting_time = 1
+
+    def __init__(self):
+        pin = (0, 0)  # Read config
+
+        # init controller
+        self.controller = TurretShootHAL(pin)
+
+        logging.info("Turret Shootingcontrol initialized")
+
+    async def on(self, seat: int = 0):
+        """
+            Shoot with the turrent when keyboardswitch is pressed
+
+            Arguments:
+                seat: number of the seat
+        """
+
+        self.controller.on()
+        sleep(self.shooting_time)
+        self.controller.off()
+
+        logging.info(f"Shot turret for seat {seat}")
 
     def shutdown(self, seat: int = 0):
         self.controller.close()
--- src/hardware/turrets.py
+++ src/hardware/turrets.py
@@ -1,8 +1,13 @@
 import logging
-import RPi.GPIO as GPIO
+from typing import Tuple
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
 
-class TurretHAL():
+class TurretPosHAL():
     """
         Hardware interface to control the turrets
 
@@ -10,14 +15,14 @@ class TurretHAL():
             pin: number of the GPIO pin used to control the turret
     """
 
-    def __init__(self, pin: int = 17):
-        self.pin = pin
+    def __init__(self, servo_pin: int = 17):
+        self.servo_pin = servo_pin
 
-        GPIO.setup(pin, GPIO.OUT)
-        self.pwm = GPIO.PWM(pin, 50)
+        GPIO.setup(servo_pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(servo_pin, 50)
         self.pwm.start(self.__mapPosToPWM__(0))
 
-        logging.info(f"Init turret at Pin {pin}")
+        logging.info(f"Init turret positioning at Pin {servo_pin}")
 
     def setPosition(self, pos: int = 50):
         """
@@ -27,7 +32,7 @@ class TurretHAL():
                 force: Turretposition between 0 and 100
         """
 
-        self.pwm.ChangeDutyCycle(self.__mapPosToPWM__(pos))
+        self.pwm.ChangeDutyCycle(self.__mapPosToPWM(pos))
         logging.info(f"Turret moved to pos {pos}")
 
     def close():
@@ -36,9 +41,9 @@ class TurretHAL():
         """
 
         GPIO.cleanup()
-        logging.info(f"Turret interface closed")
+        logging.info(f"Turret positioning interface closed")
 
-    def __mapPosToPWM__(self, pos: int) -> int:
+    def __mapPosToPWM(self, pos: int) -> int:
         """
             Map a given position to PWM duty cycle
 
@@ -55,3 +60,41 @@ class TurretHAL():
         max_pwm = pwm_range[0] + (180 / (pwm_range[1] - pwm_range[0]) * max_deg)
 
         return min_pwm + (100 / (max_pwm - min_pwm) * pos)
+
+
+class TurretShootHAL():
+    """
+        Hardware interface to control the turrets
+
+        Attributes:
+            pin: number of the GPIO pin used to control the turret
+    """
+
+    def __init__(self, led_pins: Tuple[int] = (18, 19)):
+        self.led_pins = led_pins
+
+        GPIO.setup(led_pins, GPIO.OUT, initital=GPIO.LOW)
+
+        logging.info(f"Init turret shooting at Pins {led_pins[0]}, {led_pins[1]}")
+
+    def on(self):
+        """
+            Emmit light to shoot the UFO
+        """
+
+        GPIO.output(self.led_pins, GPIO.HIGH)
+
+    def off(self):
+        """
+            Switch light emition of
+        """
+
+        GPIO.output(self.led_pins, GPIO.LOW)
+
+    def close():
+        """
+            Called when the Interface is not needed anymore
+        """
+
+        GPIO.cleanup()
+        logging.info(f"Turret shooting interface closed")
