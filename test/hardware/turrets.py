dc91ead9f921f02965f82f097a6becdfb4570622
simon_hu <simon.hu@edu.fh-joanneum.at>
2021-11-23 11:58:59 +0100

    adding gpio.setmode

--- test/hardware/turrets.py
+++ test/hardware/turrets.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+import time
+import RPi.GPIO as GPIO
+
+# for GPIO numbering, choose BCM  
+GPIO.setmode(GPIO.BCM) 
+
 from hardware.turrets import TurretHAL
 
-turret = TurretHAL()
+turret = TurretHAL(17)
 
+time.sleep(50)
+turret.setPosition(100)
 
