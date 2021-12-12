760e73b1985010fddfe51ea17509d92031f8bc0b
GeritW <gerit.wasserfaller@edu.fh-joanneum.at>
2021-12-12 10:42:38 +0000

    REWORK sensor structure

--- src/controls/sensor.py
+++ src/controls/sensor.py
@@ -1,59 +1,49 @@
-#!/usr/bin/env python3
-
-import logging
 import asyncio
-from asyncio import sleep 
-from gpiozero import Button, LED  #from gpiozero import * ?? or plus LED  or only import gpiozero 
-#from gpiozero.pins.native import NativeFactory
+from asyncio import sleep
+from gpiozero import Button, LED  
+import logging
+from game_sdk.controller.inputs import Switch
 
-
-class Sensor:
+class Sensor(Switch): 
     """
         Input class for sensor 
     """
 
-    def __init__(self):
-        self.sensor = Button(24, pull_up=True, active_state = None, bounce_time=None, pin_factory=None)  #init sensor
+    def on_init(self):
+        self.sensor = Button(24, pull_up = True, active_state = None, bounce_time = None, pin_factory= None) 
         self.schoot_led = LED(23)
         self.points_led = LED(25)
-        self. points = 0
-        print (f"Sensor initialized")
+        self.points = 0
+        logging.info("Sensor initialized")
 
     async def points_detected(self):  #example if point detected
-        self.points_led.on()
-        await asyncio.sleep(1) 
-        self.points_led.off()
-        await asyncio.sleep(1) 
+        asyncio.create_task(self.led_on(self.points_led, 1))
+
         self.points +=1
-        print (self.points)
-        print (f"Point detected")
-        #await self.close()
+        await self.game_io.score(score=self.points, seat=self.config["seat"])
 
+        logging.info("Points detected")
 
-    async def event_detected(self, schoot:bool):
-        if schoot:
-            print (f"Event")
-            self.schoot_led.on()
-            await asyncio.sleep(0.1)
-            self.schoot_led.off()
-            if self.sensor.is_pressed:
-                await self.points_detected() #if point detected 
+    async def event_detected(self):
+        asyncio.create_task(self.led_on(self.schoot_led, 0.1))
 
+        if self.sensor.is_pressed:
+            await self.points_detected() #if point detected 
+        
+        logging.info("Event")
+
+    async def led_on(self, name: str, time: float):
+        self.name.on()
+        await asyncio.sleep(time) 
+        self.name.off()
 
     async def close(self):
         self.schoot_led.close()
         self.points_led.close()
         self.sensor.close()
-        print (f"Close")
+        logging.info("Close")
 
-test_sensor = Sensor()
-#asyncio.run (test_sensor.event_detected(0))
-asyncio.run(test_sensor.event_detected(1))
-asyncio.run(test_sensor.event_detected(0))
-asyncio.run(test_sensor.event_detected(1))
-
-
-
-
-
-
+if __name__ == "__main__":
+    s = Sensor()
+    s.on_init()
+    s.event_detected()
