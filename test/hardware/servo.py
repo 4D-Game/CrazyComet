f10d21102abd15c5a4334667bab5ddaf0fcdaeb2
Lukas Schüttler <lukas.schuettler@ise-technology.com>
2022-01-26 18:53:57 +0100

    REWORK servo test

--- test/hardware/servo.py
+++ test/hardware/servo.py
@@ -12,20 +12,13 @@ try:
     logging.getLogger().setLevel(logging.DEBUG)
 
     turret1 = ServoHAL(13)
-    turret2 = ServoHAL(12)
 
-    input('Press return to stop:')
-    turret1.setPosition(-45)
-    turret2.setPosition(-45)
+    while True:
+        val = int(input('Enter Servo position in degree:'))
+        turret1.setPosition(val)
 
-    input('Press return to stop:')
-    turret1.setPosition(-90)
-    turret2.setPosition(45)
-
-    input('Press return to stop:')
 except KeyboardInterrupt:
     logging.info("Keyboard Interrupt")
 finally:
     turret1.close()
-    turret2.close()
     # GPIO.cleanup()
